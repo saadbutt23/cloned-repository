b38f17f85e419ad5e4eeb758b70e42388926ef30
Saad Butt <saadbutt23@gmail.com>
2020-12-08 00:20:23 +0100

    modularity using import and export

---
+++ 03-Browsers/2020-12-07-Modules/exercises/Browser-modules-basics-master/modules/index.js
@@ -0,0 +1,9 @@
+import {hoverHeader} from "./hover.js"
+import {scrollWindow} from "./scroll.js"
+import {clickImages} from "./click.js"
+
+
+// to fire the function 
+hoverHeader();
+scrollWindow();
+clickImages(); 
